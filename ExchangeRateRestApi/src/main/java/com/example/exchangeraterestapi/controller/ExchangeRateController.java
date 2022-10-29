066353e5e3131b7ffd2bbd59761e692e637b8bc9
ALexus <vorgen.morgen@yandex.ru>
2022-10-29 23:01:00 +0300

    Issue #4. Interface for service

--- ExchangeRateRestApi/src/main/java/com/example/exchangeraterestapi/controller/ExchangeRateController.java
+++ ExchangeRateRestApi/src/main/java/com/example/exchangeraterestapi/controller/ExchangeRateController.java
@@ -3,8 +3,6 @@ package com.example.exchangeraterestapi.controller;
 import com.example.exchangeraterestapi.model.ExchangeRate;
 import com.example.exchangeraterestapi.service.PrivateBankApiService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
---
+++ ExchangeRateRestApi/src/main/java/com/example/exchangeraterestapi/service/PrivateBankApiServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.exchangeraterestapi.service;
+
+import com.example.exchangeraterestapi.model.ExchangeRate;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class PrivateBankApiServiceImpl implements PrivateBankApiService {
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final String PB_API_URL = "https://api.privatbank.ua/p24api/pubinfo?exchange&json&coursid=11";
+
+    @Cacheable("exchangeRate")
+    public ExchangeRate[] getPBExchangeRate() {
+        return restTemplate.getForObject(PB_API_URL, ExchangeRate[].class);
+    }
+}
